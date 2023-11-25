27f070596b26c14385a1c57aba8135bfd2ba973a
Alex <zytia@mail.ru>
2023-11-25 20:27:07 +0300

    test

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 /** @var \Laravel\Lumen\Routing\Router $router */
 
-$router->post(env('TOKEN').'/webhook', 'TelegramController@handle');
+$router->post('/', 'TelegramController@handle');
 
 $router->get('/', function () {
     return 'а что ты тут ожидал увидеть?)';
